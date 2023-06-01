df1726e6b8685257b7b2b1bbbb99d8f002e26c38
Dabel04 <134778288+Dabel04@users.noreply.github.com>
2023-06-01 11:38:09 +0100

    DABEL CODE GOU/U21/CSC/748

---
+++ dabel peter.java
@@ -0,0 +1,15 @@
+jLabel1.setText("Create an account");
+
+        jLabel2.setText("ID type");
+
+        jLabel3.setText("Select ID number");
+
+        jLabel4.setText("Password");
+
+        jLabel5.setText("Username");
+
+        jLabel6.setText("Date of Birth");
+
+        jLabel7.setText("E-mail");
+
+        jLabel8.setText("Phone");
